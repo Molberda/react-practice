860614076d0d4d64595ac1d3a488a47a9c6c636c
David <davidmollera@hotmail.com>
2022-11-12 19:08:30 -0500

    first approach at routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,20 @@
 import "./App.css";
-import Todo from "./components/Todo.jsx";
-import Title from "./components/Title.jsx";
-import Modal from "./components/Modal.jsx";
-import React, { useState } from "react";
-// import Counter from "./components/Counter.jsx"
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home.jsx'
+import About from "./pages/About";
+import Contact from "./pages/Contact";
 
 function App() {
-  const [showModal, setShowModal] = useState(false);
-
-  function confirmDelete(){
-    setShowModal(true)
-  }
-
-  function closeModal(){
-    setShowModal(false)
-    console.log('cancel')
-  }
-
-  function confirmModal(){
-    setShowModal(false)
-    console.log('confirm')
-  }
 
   return (
     <div>
-      <Title title="My ToDo List" />
-      <input type="text" />
-      <button>Add todo</button>
-      <div className="todo__wrapper">
-        <Todo confirm={confirmDelete} title="Finish Frontend" button="Done"/>
-        <Todo confirm={confirmDelete} title="Finish Interview Section" button="In Progress" />
-        <Todo confirm={confirmDelete} title="Land a $100K Job" button="Next Up" />
-      </div>
-      {showModal && <Modal close = {closeModal} confirm = {confirmModal} question="Are you sure?" />}
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/about" element={<About/>}/>
+          <Route path="/contact" element={<Contact/>}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
