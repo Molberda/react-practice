4ac00ebdd2b89c3c603b15ebfc3ad4691b5e1377
David <davidmollera@hotmail.com>
2022-11-13 21:58:25 -0500

    started using dynamic routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import "./App.css";
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Home from './pages/Home.jsx'
-import About from "./pages/About";
-import Contact from "./pages/Contact";
+// import About from "./pages/About";
+// import Contact from "./pages/Contact";
 import Nav from "./components/Nav";
+import Users from "./pages/Users.jsx";
 
 function App() {
 
@@ -12,8 +13,7 @@ function App() {
         <Nav/>
         <Routes>
           <Route path="/" element={<Home />}/>
-          <Route path="/about" element={<About/>}/>
-          <Route path="/contact" element={<Contact/>}/>
+          <Route path="/users/:username" element={<Users />}/>
         </Routes>
       </Router>
   );
