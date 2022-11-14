9f972336833c767c2dd1298210e259ecdb5b9a51
David <davidmollera@hotmail.com>
2022-11-14 13:24:12 -0500

    first approach at api integration with react

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,16 @@
+import React, { useEffect } from 'react'
+import axios from 'axios'
+
 function Home(){
+    async function fetchData(){
+        const { data } = await axios.get("https://jsonplaceholder.typicode.com/users/")
+        console.log(data)
+    }
+
+    useEffect(() => {
+        fetchData()
+    }, [])
+    
     return <h1>Home</h1>
 }
 
